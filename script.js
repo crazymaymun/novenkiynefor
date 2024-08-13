85962cd7a9a8abb2147ec814e7802ef7d69ceac7
crazymaymun <mmirha965@gmail.com>
2024-08-13 21:02:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+function ruslanjonka() {
+    var paq = Math.floor(Math.random() * val)
+    console.log(paq);
+    if (paq == 1) {
+      document.getElementById("demo").innerHTML="Черный"
+    }else if(paq == 2){
+      document.getElementById("demo").innerHTML="Смуглый"
+    }else if(paq == 3){
+      document.getElementById("demo").innerHTML="Белый"
+    }
+    }
+  let val = 14
+  function elenaamadzhanovna() {
+    var gibdd = Math.floor(Math.random() * val)
+    console.log(gibdd);
+    if (gibdd == 1) {
+      document.getElementById("robiyahon").innerHTML="155"
+    }else if(gibdd == 2){
+      document.getElementById("robiyahon").innerHTML="160"
+    }else if(gibdd == 3){
+      document.getElementById("robiyahon").innerHTML="165"
+    }else if(gibdd == 2){
+        document.getElementById("robiyahon").innerHTML="170"
+    }else if(gibdd == 3){
+        document.getElementById("robiyahon").innerHTML="175"
+    }else if(gibdd == 3){
+        document.getElementById("robiyahon").innerHTML="180"
+    }else if(gibdd == 2){
+          document.getElementById("robiyahon").innerHTML="185"
+    }else if(gibdd == 2){
+        document.getElementById("robiyahon").innerHTML="190"
+      }else if(gibdd == 3){
+        document.getElementById("robiyahon").innerHTML="195"
+    }
+    }
+    function elenaarkadyevna() {
+        var gibdd = Math.floor(Math.random() * val)
+        console.log(gibdd);
+        if (gibdd == 1) {
+          document.getElementById("finlyandiya").innerHTML="40"
+        }else if(gibdd == 2){
+          document.getElementById("finlyandiya").innerHTML="45"
+        }else if(gibdd == 3){
+          document.getElementById("finlyandiya").innerHTML="50"
+        }else if(gibdd == 2){
+            document.getElementById("finlyandiya").innerHTML="55"
+        }else if(gibdd == 3){
+            document.getElementById("finlyandiya").innerHTML="60"
+        }else if(gibdd == 3){
+            document.getElementById("finlyandiya").innerHTML="65"
+        }else if(gibdd == 2){
+              document.getElementById("finlyandiya").innerHTML="70"
+        }else if(gibdd == 2){
+            document.getElementById("finlyandiya").innerHTML="80"
+          }else if(gibdd == 3){
+            document.getElementById("finlyandiya").innerHTML="85"
+        }else if(gibdd == 3){
+            document.getElementById("finlyandiya").innerHTML="90"
+        }
+        }
+
+        function soskaristan() {
+            var paq = Math.floor(Math.random() * val)
+            console.log(paq);
+            if (paq == 1) {
+              document.getElementById("amirajonka").innerHTML="узкие"
+            }else if(paq == 2){
+              document.getElementById("amirajonka").innerHTML="обычные"
+            }
+            }
+            function kitaysila() {
+                var paq = Math.floor(Math.random() * val)
+                console.log(paq);
+                if (paq == 1) {
+                  document.getElementById("balerina").innerHTML="карие"
+                }else if(paq == 2){
+                  document.getElementById("balerina").innerHTML="голубые"
+                }
+                }
+            
+  // document.getElementById("demo").innerHTML=""
+  // var gibdd = Math.floor(Math.random() * val)
+  // console.log(paq);
