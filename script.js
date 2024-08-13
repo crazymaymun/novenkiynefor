312e658ab7bae074a691bed903bbf4f6834a394f
crazymaymun <mmirha965@gmail.com>
2024-08-13 21:14:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,80 @@
 function ruslanjonka() {
     var paq = Math.floor(Math.random() * val)
     console.log(paq);
-    if (paq == 1) {
+    if (paq == 0) {
       document.getElementById("demo").innerHTML="Черный"
-    }else if(paq == 2){
+    }else if(paq == 1){
       document.getElementById("demo").innerHTML="Смуглый"
-    }else if(paq == 3){
+    }else if(paq == 2){
       document.getElementById("demo").innerHTML="Белый"
     }
     }
-  let val = 14
+  let val = 2
   function elenaamadzhanovna() {
-    var gibdd = Math.floor(Math.random() * val)
+    var gibdd = Math.floor(Math.random() * vall )
     console.log(gibdd);
-    if (gibdd == 1) {
+    if (gibdd == 0) {
       document.getElementById("robiyahon").innerHTML="155"
-    }else if(gibdd == 2){
+    }else if(gibdd == 1){
       document.getElementById("robiyahon").innerHTML="160"
-    }else if(gibdd == 3){
+    }else if(gibdd == 2){
       document.getElementById("robiyahon").innerHTML="165"
-    }else if(gibdd == 2){
+    }else if(gibdd == 3){
         document.getElementById("robiyahon").innerHTML="170"
-    }else if(gibdd == 3){
+    }else if(gibdd == 4){
         document.getElementById("robiyahon").innerHTML="175"
-    }else if(gibdd == 3){
+    }else if(gibdd == 5){
         document.getElementById("robiyahon").innerHTML="180"
-    }else if(gibdd == 2){
+    }else if(gibdd == 6){
           document.getElementById("robiyahon").innerHTML="185"
-    }else if(gibdd == 2){
+    }else if(gibdd == 7){
         document.getElementById("robiyahon").innerHTML="190"
-      }else if(gibdd == 3){
+      }else if(gibdd == 8){
         document.getElementById("robiyahon").innerHTML="195"
     }
+let valll = 9;
     }
     function elenaarkadyevna() {
-        var gibdd = Math.floor(Math.random() * val)
+        var gibdd = Math.floor(Math.random() * vall)
         console.log(gibdd);
-        if (gibdd == 1) {
+        if (gibdd == 0) {
           document.getElementById("finlyandiya").innerHTML="40"
-        }else if(gibdd == 2){
+        }else if(gibdd == 1){
           document.getElementById("finlyandiya").innerHTML="45"
-        }else if(gibdd == 3){
+        }else if(gibdd == 2){
           document.getElementById("finlyandiya").innerHTML="50"
-        }else if(gibdd == 2){
+        }else if(gibdd == 3){
             document.getElementById("finlyandiya").innerHTML="55"
-        }else if(gibdd == 3){
+        }else if(gibdd == 4){
             document.getElementById("finlyandiya").innerHTML="60"
-        }else if(gibdd == 3){
+        }else if(gibdd == 5){
             document.getElementById("finlyandiya").innerHTML="65"
-        }else if(gibdd == 2){
+        }else if(gibdd == 6){
               document.getElementById("finlyandiya").innerHTML="70"
-        }else if(gibdd == 2){
+        }else if(gibdd == 7){
             document.getElementById("finlyandiya").innerHTML="80"
-          }else if(gibdd == 3){
+          }else if(gibdd == 8){
             document.getElementById("finlyandiya").innerHTML="85"
-        }else if(gibdd == 3){
+        }else if(gibdd == 9){
             document.getElementById("finlyandiya").innerHTML="90"
         }
         }
-
+let vall = 9;
         function soskaristan() {
             var paq = Math.floor(Math.random() * val)
             console.log(paq);
-            if (paq == 1) {
+            if (paq == 0) {
               document.getElementById("amirajonka").innerHTML="узкие"
-            }else if(paq == 2){
+            }else if(paq == 1){
               document.getElementById("amirajonka").innerHTML="обычные"
             }
             }
             function kitaysila() {
                 var paq = Math.floor(Math.random() * val)
                 console.log(paq);
-                if (paq == 1) {
+                if (paq == 0) {
                   document.getElementById("balerina").innerHTML="карие"
-                }else if(paq == 2){
+                }else if(paq == 1){
                   document.getElementById("balerina").innerHTML="голубые"
                 }
                 }
